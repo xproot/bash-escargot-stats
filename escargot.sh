c81804119f4f932545705e4b5f76c6c174ba044d
ThatXPUser <xpusergot@gmail.com>
2020-11-28 17:30:41 -0500

    Add spaces to make the code look clean
    ¯\_(ツ)_/¯

--- escargot.sh
+++ escargot.sh
@@ -5,29 +5,40 @@
 
 #tell the user the script is actually running.
 echo "Working..."
+
 #Calculating the epoch s and ns to ms i believe and making it start var
 start=`echo $(($(date +%s%N)/1000000))`
+
 #Netcat to escargot and wait for response
 output=`echo 'VER 1 XP' | nc -w 15 m1.escargot.log1p.xyz 1863`
+
 #Calculating the epoch s and ns to ms i believe and making it end var
 end=`echo $(($(date +%s%N)/1000000))`
+
 #calculaling the time taken substracting new epoch from old epoch
 ms=$((end-start))
+
 #if's to check if its congested or not
+
 #we start putting online
 status="Online :D"
+
 #if its more than 4999ms then its congested
 if [ $ms -gt 4999 ]; then
-status="Congested :/"
+ status="Congested :/"
 fi
+
 #if its more than 14999ms then its offline (nc will timeout around now)
 if [ $ms -gt 14999 ]; then
-status="Offline :("
+ status="Offline :("
 fi
+
 #checking how many characters are in the ms var
 grep=`grep -o "[1|2|3|4|5|6|7|8|9|0]" <<<"$ms" | wc -l`
+
 #case for splicing
 timetaken="If this shows up, tell the creator something is wrong."
+
 case $grep in
      4)
 	  # X seconds, XXX ms.
@@ -48,10 +59,12 @@ case $grep in
 esac
 
 #finally output what we know
+
 echo "=============ESCARGOT STATUS============="
 echo "Is: $status"
 echo "Time taken: $timetaken."
 echo "=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/="
+
 #info
 echo "Accepted normal server response times are"
 echo "5 seconds or less, server is considered"
